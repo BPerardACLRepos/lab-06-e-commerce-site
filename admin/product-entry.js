7d5e53ffee052e15569f9f0f4e2fe6488adf63a2
Brandon Perard <brandon.perard@gmail>
2021-01-25 10:48:50 -0800

    update product-entry.js to generate unique id from current product data and set price as float type

--- admin/product-entry.js
+++ admin/product-entry.js
@@ -1,14 +1,26 @@
+import { books } from '../data/book-list.js';
+
 const productEntryForm = document.querySelector('#product-entry');
 
 productEntryForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const newProduct = [];
+    const productTraits = {};
+    const newId = books.length + 1;
+    productTraits.id = newId;
 
     const formData = new FormData(productEntryForm);
     const entries = formData.entries();
     for (let field of entries) {
-        newProduct.push(`{${field[0]}: field[1]}`);
+        if (field[0] === 'price') {
+            productTraits[field[0]] = Number(field[1]);
+        } else {
+            productTraits[field[0]] = field[1];
+        }
+
     }
-    console.log(newProduct);
+    console.log(productTraits);
+    console.log(books);
+    books.push(productTraits);
+    console.log(books);
 });
